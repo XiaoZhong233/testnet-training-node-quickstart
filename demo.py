d11185d23cad29d15716aee7c68333aef6befa23
howell <xiaozhonggis@gmail.com>
2025-01-13 10:13:42 +0800

    使用flash attention 2

--- demo.py
+++ demo.py
@@ -26,7 +26,7 @@ def train_lora(
     assert model_id in model2template, f"model_id {model_id} not supported"
     lora_config = LoraConfig(
         r=training_args.lora_rank,
-        target_modules=["q_proj", "v_proj"],
+        target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
         lora_alpha=training_args.lora_alpha,
         lora_dropout=training_args.lora_dropout,
         task_type="CAUSAL_LM",
@@ -57,11 +57,13 @@ def train_lora(
         model_id,
         use_fast=True,
     )
+    tokenizer.padding_side = "right"
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
         quantization_config=bnb_config,
         device_map={"": 0},
         token=os.environ["HF_TOKEN"],
+        attn_implementation="flash_attention_2"
     )
 
     # Load dataset
